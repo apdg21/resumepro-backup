a92bd8814ef1f6280f77a137522a6b273c74d0a9
apdg21 <floda2114@gmail.com>
2026-01-23 23:12:42 +0800

    Delete tools directory

--- tools/assets/js/clean-urls.js
+++
@@ -1,9 +0,0 @@
-function cleanUrl() {
-    const currentUrl = window.location.pathname;
-    if (currentUrl.endsWith('.html')) {
-        const cleanPath = currentUrl.replace(/\.html$/, '');
-        history.replaceState(null, '', cleanPath);
-    }
-}
-
-window.addEventListener('load', cleanUrl);
